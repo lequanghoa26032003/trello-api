e45dfc446be780d3cd773aaea3e8a8e7bae15153
lequanghoa26032003 <117667583+lequanghoa26032003@users.noreply.github.com>
2024-10-22 17:39:29 +0700

    bai63

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,22 +1,17 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
-
+import { BOARD_TYPES } from '~/utils/constants'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict(),
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required()
   })
   try {
-
     // console.log('req.body', req.body)
-
     await correctCondition.validateAsync(req.body, { abortEarly: false })
     next()
 
