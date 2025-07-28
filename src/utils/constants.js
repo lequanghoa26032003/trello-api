cdab3ed5a7ec69cea32b69fb6a69c72050d944ef
lequanghoa26032003 <117667583+lequanghoa26032003@users.noreply.github.com>
2025-07-28 09:36:21 +0700

    update cros

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 import { env } from '~/config/environment'
 
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173'
+]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
   PRIVATE: 'private'
